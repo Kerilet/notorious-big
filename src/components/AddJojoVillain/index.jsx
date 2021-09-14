58032d6d61f298958ba228e10fe0ac76385d802b
Jesse Pinheiro <jessebpinheiro@hotmail.com>
2021-09-13 22:05:26 -0300

    fix errors

--- src/components/AddJojoVillain/index.jsx
+++ src/components/AddJojoVillain/index.jsx
@@ -1,29 +1,35 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React, { useState } from 'react';
-import { InputGroup, FormControl, Button, Form } from 'react-bootstrap';
+import { InputGroup, FormControl, Button, Form, Col, Row } from 'react-bootstrap';
 
 export default (props) => {
 
-    const [text, setText] = useState('');
-    const [imageUrl, setImageURL] = useState('');
+    const [name, setName] = useState('');
+    const [image, setImage] = useState('');
+    const [isAlive, setIsAlive] = useState(false);
 
     const submit = (ev) => {
         ev.preventDefault();
-        if (text) {
-            props.onAddName(text);
-            setText('');
-        }
-        if (imageUrl) {
-            props.onAddImage(imageUrl);
-            setImageURL('');
+        if (name && image) {
+          props.onAddVillain({
+            name,
+            image,
+          });
         }
     }
 
     return <Form onSubmit={submit}>
+          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+            <Form.Label column sm="2">
+              Nome
+            </Form.Label>
+            <Col sm="10">
+              <FormControl onChange={(ev) => setName(ev.target.value)} placeholder="Villain name" value={name} />
+            </Col>
+          </Form.Group>
             <InputGroup className="mb-3">
-                <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-                <FormControl onChange={(ev) => setText(ev.target.value)} value={text} />
-                <FormControl onChange={(ev) => setImageURL(ev.target.value)} value={imageUrl} />
+            <Form.Check type="switch" label="Is alive" onChange={(ev) => setIsAlive(ev.target.checked)} value={isAlive} />
+                <FormControl onChange={(ev) => setImage(ev.target.value)} value={image} />
                 <Button type="submit">Add</Button>
             </InputGroup>
         </Form>;
